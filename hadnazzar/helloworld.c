06df3212bc85c7d16c92569a585f191e0db31a91
Melih Yumak <hadnazzar@users.noreply.github.com>
2018-10-01 16:59:44 +0200

    Hadnazzar c (#25)
    * Hadnazzar python hello world added
    
    * C code added

---
+++ hadnazzar/helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+   printf("🎃 Hello World - 2018 Hacktoberfest! 🎃   ");
+   return 0;
+}
+
+// For running on OSX
+// make helloworld
+// Then run 
+// ./helloworld
---
+++ hadnazzar/helloworld.py
@@ -0,0 +1,5 @@
+print("Hello World - 2018 Hacktoberfest!")
+
+# Python 2.7
+# 
+# Run python helloworld.py
